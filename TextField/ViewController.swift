3ab1c9eb0d0e46f26877b2c9b9fd866079f3d51b
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-02 15:52:21 +0900

    TextField

--- TextField/ViewController.swift
+++ TextField/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //delegate 연결
+        myTextField.delegate = self
+        myTextField.clearButtonMode = UITextFieldViewMode.always
+        myTextField.placeholder = "입력하세요!"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(_ sender: Any) {
+        myLabel.text = myTextField.text
+        myTextField.text = ""
+        myTextField.resignFirstResponder()
+        //버튼을 실행시키면 키보드를 내린다.
     }
-
-
+    
+    //UItextFieldDelegate method
+    //called when 'return' key pressed. return No to ignore.
+    func  textFieldShouldReturn(_ textField: UITextField) ->
+        Bool {
+        print("I pressed return key")
+        //버튼을 실행시키면 키보드를내림
+        myTextField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
 }
 
